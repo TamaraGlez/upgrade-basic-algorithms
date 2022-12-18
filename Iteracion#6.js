8b555fffac81f35c06ba590e430624283558bdc9
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2022-12-18 14:58:21 +0100

    Iteracion#6

--- Iteracion#6.js
+++ Iteracion#6.js
@@ -1,4 +1,16 @@
 // 6.1
 for (let i = 0; i <= 9; i++) {
     console.log(i);
+// 6.2
+    if (i % 2 == 0) {
+    console.log("números pares:", i)
+    }
+}
+// 6.3
+for (let i = 0; i <= 10; i++) {
+    if (i === 10 ) {
+    console.log('Dormido!');
+    } else{
+    console.log('Intentando dormir ');
+    }
 }
